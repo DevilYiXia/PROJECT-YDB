e4da45eb59e1c65146d3360c7265ed6b3ffe3800
unknown <1603648278@qq.com>
2022-01-04 18:17:57 +0800

    项目文件

--- js/quehuan.js
+++ js/quehuan.js
@@ -34,25 +34,32 @@ $(function(){
 									controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 						});
 
-              /*返回顶部*/
-              //绑定滚动事件
-              $(document).scroll(function(){
-              	//获取距离顶部的位置
-              	var topDistance = $('html,body').scrollTop();
-              	//判断
-              	if(topDistance > 500){
-              		$('.backToTop').fadeIn();
-              	} else {
-              		$('.backToTop').fadeOut();
-            }
-                }) 
-                //返回顶部功能
-                $('.backToTop').click(function(){
-              	   $('html,body').animate({
-              		   scrollTop:0
-              	   })
-                })
 			
-			
-			
+			/*新书列表手风琴效果*/
+			$('.ebooks .right-box ul > li').mouseenter(function(){
+				//所有兄弟：隐藏详情 显示标题
+				$(this).sibligs().find('.desc').hide();
+				$(this).sibligs().find('.ebooks-title').show();
+				//当前：隐藏标题 显示详情
+				$(this).sibligs().find('.ebooks-title').hide();
+				$(this).sibligs().find('.desc').show();
+			})
+			/*返回顶部*/
+			 //绑定滚动事件
+			$(document).scroll(function(){
+			    //获取距离顶部的位置
+			    var topDistance = $('html,body').scrollTop();
+			    //判断
+			    if(topDistance > 500){
+			        $('.backToTop').fadeIn();
+			    } else {
+			        $('.backToTop').fadeOut();
+			    }
+			    }) 
+			      //返回顶部功能
+			     $('.backToTop').click(function(){
+			        $('html,body').animate({
+			            scrollTop:0
+			         },300)
+			    })
 })
